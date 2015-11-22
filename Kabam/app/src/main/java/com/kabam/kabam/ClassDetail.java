d883c1a6170eb061781e34af83ca105f42439fc8
Alex Leung <leungale@usc.edu>
2015-11-22 15:40:34 -0800

    Added enroll functionality

--- Kabam/app/src/main/java/com/kabam/kabam/ClassDetail.java
+++ Kabam/app/src/main/java/com/kabam/kabam/ClassDetail.java
@@ -14,6 +14,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.ParseUser;
+
 import java.util.List;
 import java.util.Set;
 
@@ -24,10 +26,73 @@ public class ClassDetail extends Fragment {
 
     private Class selectedClass;
     private EventQueryAdapter classEvents;
+    private Boolean enrolled = false;
+    private ListView classDetailList;
+    private View enrollButton, eventButton, addEventButton, chatButton, addChatButton;
+
+    private void notLoggedIn() {
+        enrollButton.setVisibility(View.GONE);
+        eventButton.setVisibility(View.GONE);
+        addEventButton.setVisibility(View.GONE);
+        chatButton.setVisibility(View.GONE);
+        addChatButton.setVisibility(View.GONE);
+        classDetailList.setVisibility(View.GONE);
+    }
+
+    private void notEnrolled() {
+        enrollButton.setVisibility(View.VISIBLE);
+        eventButton.setVisibility(View.GONE);
+        addEventButton.setVisibility(View.GONE);
+        chatButton.setVisibility(View.GONE);
+        addChatButton.setVisibility(View.GONE);
+        classDetailList.setVisibility(View.GONE);
+    }
+
+    private void enrolled() {
+        enrollButton.setVisibility(View.GONE);
+        classDetailList.setVisibility(View.VISIBLE);
+        eventButton.setVisibility(View.VISIBLE);
+        addEventButton.setVisibility(View.VISIBLE);
+        chatButton.setVisibility(View.VISIBLE);
+        addChatButton.setVisibility(View.VISIBLE);
+    }
+
+    private void refreshButtons() {
+        if (ParseUser.getCurrentUser() != null) {
+            if (enrolled) {
+                enrolled();
+            } else {
+                notEnrolled();
+                enrollButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        enrollButton.setVisibility(View.GONE);
+                        enrolled = true;
+                        refreshButtons();
+                    }
+                });
+            }
+        } else {
+            notLoggedIn();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        refreshButtons();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.class_detail, container, false);
+
+        classDetailList = (ListView) view.findViewById(R.id.classDetailList);
+        enrollButton = view.findViewById(R.id.enrollButton);
+        eventButton = view.findViewById(R.id.eventButton);
+        addEventButton = view.findViewById(R.id.addEventButton);
+        chatButton = view.findViewById(R.id.chatButton);
+        addChatButton = view.findViewById(R.id.addChatButton);
 
         if (getArguments() != null) {
             String classID = getArguments().getString("class");
@@ -36,11 +101,13 @@ public class ClassDetail extends Fragment {
             getActivity().getSupportFragmentManager().popBackStack();
         }
 
+        refreshButtons();
+
         if (selectedClass != null) {
             ((TextView)view.findViewById(R.id.className)).setText(selectedClass.getClassTitle());
             ((TextView)view.findViewById(R.id.classEnrolled)).setText(selectedClass.getEnrollCount());
             classEvents = new EventQueryAdapter(getActivity(), selectedClass);
-            ((ListView)view.findViewById(R.id.classDetailList)).setAdapter(classEvents);
+            classDetailList.setAdapter(classEvents);
         }
 
         view.findViewById(R.id.eventButton).setOnClickListener(new View.OnClickListener() {
@@ -52,7 +119,7 @@ public class ClassDetail extends Fragment {
             }
         });
 
-        view.findViewById(R.id.addEventButton).setOnClickListener(new View.OnClickListener() {
+        addEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (selectedClass != null) {
@@ -69,14 +136,14 @@ public class ClassDetail extends Fragment {
             }
         });
 
-        view.findViewById(R.id.chatButton).setOnClickListener(new View.OnClickListener() {
+        chatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
 
-        view.findViewById(R.id.addChatButton).setOnClickListener(new View.OnClickListener() {
+        addChatButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (selectedClass != null) {
