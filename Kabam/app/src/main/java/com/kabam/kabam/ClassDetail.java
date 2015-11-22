ed1e14ce952de042f47e6c649669d007996b35e2
Alex Leung <leungale@usc.edu>
2015-11-21 20:44:05 -0800

    Added ClassDetail

--- Kabam/app/src/main/java/com/kabam/kabam/ClassDetail.java
+++ Kabam/app/src/main/java/com/kabam/kabam/ClassDetail.java
@@ -1,7 +1,57 @@
 package com.kabam.kabam;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
 /**
- * Created by Ayush on 11/19/15.
+ * Created by Alex on 11/19/15.
  */
-public class ClassDetail {
+public class ClassDetail extends Fragment {
+    String title, enrolled;
+
+    public ClassDetail() {
+
+    }
+
+    public ClassDetail(Class c) {
+        this.title = c.getClassTitle();
+        this.enrolled = c.getEnrollCount();
+    }
+
+    public void onEventClick(View view) {
+
+    }
+
+    public void onChatClick(View view) {
+
+    }
+
+    public void onAddEventClick(View view) {
+
+    }
+
+    public void onAddChatClick(View view) {
+
+    }
+
+    public static final ClassDetail newClassDetail(Class c) {
+        ClassDetail cd = new ClassDetail(c);
+        Bundle bundle = new Bundle(2);
+        bundle.putString("title", c.getClassTitle());
+        bundle.putString("enrolled", c.getEnrollCount());
+        cd.setArguments(bundle);
+        return cd;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.class_detail, container, false);
+        ((TextView) view.findViewById(R.id.className)).setText(getArguments().getString("title"));
+        ((TextView) view.findViewById(R.id.classEnrolled)).setText(getArguments().getString("enrolled"));
+        return view;
+    }
 }
